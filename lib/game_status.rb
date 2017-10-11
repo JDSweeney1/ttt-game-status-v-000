9c37e8e3b073684ad37e957d7b3e39634714ba89
Jered Sweeney <sweeneyjered@gmail.com>
2017-10-11 16:21:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   if WIN_COMBINATIONS.select do |win_combination|
       (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") == true
-  else 
+  else
     false
   end
 end
